88bc9a199a91ac5254961166212a3e9c6d7b4b1a
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-11-30 21:40:12 +0000

    update def

--- alisa_library/create.py
+++ alisa_library/create.py
@@ -3,19 +3,16 @@ from youtubesearchpython import *
 def viedo_suggestions(search):
     '''Get the youtube video suggestion'''
     viedo_suggestions = Suggestions(language = 'en', region = 'US')
-    viedo_suggestions=viedo_suggestions.get('search', mode = ResultMode.json)
+    viedo_suggestions=viedo_suggestions.get(search, mode = ResultMode.json)
     return viedo_suggestions
 
 
 def channels_Search(search):
     '''Search only channels'''
-    ChannelsSearch=ChannelsSearch('search', limit = 6, region = 'US')
+    ChannelsSearch=ChannelsSearch(search, limit = 6, region = 'US')
     return channels_Search.result()
 
 def playlists_Search(search):
     '''Search only playlists'''
-    playlists_Search = PlaylistsSearch('search', limit = 2)
+    playlists_Search = PlaylistsSearch(search, limit = 2)
     return playlists_Search.result()
-
-
-viedo_suggestions(search)
